da687607d1ad7cdb18f2f9a2ce8a19898f11e9af
Pankaj <pankajdevrath15@gmail.com>
2018-06-19 16:03:24 +0530

    commit

--- Assignment_4/SortAnArray.java
+++ Assignment_4/SortAnArray.java
@@ -17,7 +17,7 @@ public class SortAnArray {
 	
 	
 	//Merge sort(Complexity O(nlon(n))
-	  public static void merge(int arr[],int min,int mid,int max)
+	/*  public static void merge(int arr[],int min,int mid,int max)
 	    {
 	        int n1=mid-min+1;
 	        int n2=max-mid;
@@ -50,14 +50,34 @@ public class SortAnArray {
 	            sort(arr,mid+1,max);
 	            merge(arr,min,mid,max);
 	        }
-	    }
+	    }*/
+	    
 	   public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int arr[]=new int[] {1,1,1,1,0,0,1,0};
 		int length=arr.length;
 		
-		sort(arr,0,length-1);
-		for(int i=0;i<length;i++)
-			System.out.println(arr[i]);
+		// sort(arr,0,length-1);
+// 		for(int i=0;i<length;i++)
+// 			System.out.println(arr[i]);
+	  int arr[]={1,1,1,1,0,0,1,0};
+        int length=arr.length;
+      int temp;
+
+      int j=0;
+        for(int i=0;i<length;i++)
+        {
+            if(arr[i]==0)
+            {
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+
+        }
+
+        for(int i=0;i<length;i++)
+            System.out.println(" "+arr[i]);
 		}
 }
